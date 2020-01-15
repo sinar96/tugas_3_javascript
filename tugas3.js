7181f56d69d81692dbfd6552dc029b4f62357727
sinar96 <sinar96>
2020-01-15 20:40:13 +0800

    Learn about function

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+// Inisialisasi fungsi dengan parameter x
+function fungsi(x = 4) {
+  // Inisialisasi y sebagai x
+  var y = x;
+  // nilai balik
+  return y;
+}
+// mengirim nilai
+console.log(fungsi(6));
+// menampilkan nilai yang sdh dirubah
+console.log(fungsi());
